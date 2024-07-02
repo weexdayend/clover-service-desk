d280f341e24a57e84dd405675d5a2a3a3b99b396
weexdayend <weexdayend@gmail.com>
2024-07-02 13:31:01 +0700

    finishin tweaks

--- src/server/services/user.ts
+++ src/server/services/user.ts
@@ -9,7 +9,7 @@ async function authenticate(username: string, password: string) {
     throw new Error('Username and password are required');
   }
 
-  const response = await axios.post('http://localhost:3000/api/auth/sign-in', {
+  const response = await axios.post('http://localhost:7654/api/auth/sign-in', {
     username,
     password
   });
